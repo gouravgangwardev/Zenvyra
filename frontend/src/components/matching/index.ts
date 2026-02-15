f4aedc69cd0477447a13962a5cd7f3aeeb328b43
gouravgangwardev <gangwargourav24@gmail.com>
2026-02-15 22:47:20 +0530

    Create index.ts

---
+++ frontend/src/components/matching/index.ts
@@ -0,0 +1,7 @@
+// src/components/matching/index.ts
+export { default as MatchingScreen } from './MatchingScreen';
+export type { MatchingScreenProps, MatchingState } from './MatchingScreen';
+
+export { default as QueueStatus } from './QueueStatus';
+export type { QueueStats, QueueType } from './QueueStatus';
+
