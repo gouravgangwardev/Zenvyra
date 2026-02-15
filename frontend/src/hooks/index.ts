cd9ac8052d1e729d9f1f3de7453efe59a84e3ff0
gouravgangwardev <gangwargourav24@gmail.com>
2026-02-15 22:50:14 +0530

    Create index.ts

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,5 @@
+// src/hooks/index.ts
+export { useAuth } from './useAuth';
+export { useSocket, useSocketEvent, usePresence } from './useSocket';
+export { useWebRTC } from './useWebRTC';
+export { useChat } from './useChat';
